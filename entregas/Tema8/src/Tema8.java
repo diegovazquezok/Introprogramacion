a8828fd0a422e6d533c4ff3fbbc3513b20f8ed21
Diego Vazquez <diegovazquezok@gmail.com>
2023-04-21 21:49:44 -0300

    Entrega tema 8

---
+++ entregas/Tema8/src/Tema8.java
@@ -0,0 +1,18 @@
+public class Tema8 {
+
+    public class Tema8 {
+        public static void main(String[] args) {
+            Persona persona = new Persona();
+
+            persona.setEdad(44);
+            persona.setNombre("Diego");
+            persona.setTelefono("46010522");
+
+            System.out.println("Nombre: " + persona.getNombre());
+            System.out.println("Edad: " + persona.getEdad());
+            System.out.println("Teléfono: " + persona.getTelefono());
+        }
+
+
+    }
+}
